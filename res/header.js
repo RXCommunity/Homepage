09b037a5c72a5009abfb0d3af798e7708f790de3
caskd <caskd@gmx.de>
2018-06-13 19:12:17 +0200

    Tweaked stuff around

--- res/header.js
+++ res/header.js
@@ -1,10 +1,11 @@
 window.onscroll = function() {checkheader()};
 
 // Get the header
-var header = document.getElementById("myHeader");
+var header = document.getElementById("pgHeader");
+var banner = document.getElementById("text");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = banner.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function checkheader() {
