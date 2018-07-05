5cc50968c4ee398a2f341f4ab7f195970fe34237
caskd <caskd@gmx.de>
2018-07-05 18:19:19 +0200

    Some fixes and making it look nice

--- js/main.js
+++ js/main.js
@@ -1,18 +1,11 @@
 $(document).ready(function() {
-  /* Load Google Analytics */
-  $.getScript("https://www.googletagmanager.com/gtag/js?id=UA-120536610-1");
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-120536610-1');
-});
-
-$(document).scroll(function() {
-  /* On scroll check if scrolled beyond 100% */
-  var header = document.getElementById("pgHeader");
-  if (window.pageYOffset >= $(window).height()) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+  $(document).scroll(function() {
+    /* On scroll check if scrolled beyond 100% */
+    var header = document.getElementById("pgHeader");
+    if (window.pageYOffset >= $(window).height()) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  });
 });
