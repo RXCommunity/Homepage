0718336402b73b8ad8b69a875bbf5c77ba705bce
caskd <caskd@gmx.de>
2018-08-01 12:30:57 +0200

    Keep git repo up to date

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
-$(document).ready(function() {
-  $(document).scroll(function() {
-    /* On scroll check if scrolled beyond 100% */
-    var header = document.getElementById("pgHeader");
-    if (window.pageYOffset >= $(window).height()) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
-  });
+$(document).ready(function () {
+    $("head").append(
+        '<link rel="stylesheet" href="css/rules.css">' +
+        '<link rel="stylesheet" href="css/responsive.css">' +
+        '<link rel="stylesheet" href="css/animations.css">'
+    );
+    $('#abt-btn').click(function () {
+        $('#abt').toggleClass('hide');
+        $('.h-hide').toggleClass('h-on');
+        LoadOnce();
+    });
+    $('#close').click(function () {
+        $('#abt').addClass('hide');
+        $('.h-hide').removeClass('h-on');
+    });
+    $('.tab').click(function () {
+        var tab_id = $(this).attr('data-tab');
+        $('div.tabs a').removeClass('chosen');
+        $('.tab-content').addClass('hide');
+        $(this).addClass('chosen');
+        $('#' + tab_id).removeClass('hide');
+    });
+});
+
+function LoadOnce() {
+    $('#content').load('ajax/about.html');
+    $('.tab-content').addClass('hide');
+    LoadOnce = function () {};
+}
+
+var scene = document.getElementById('bg');
+var parallaxInstance = new Parallax(scene, {
+    relativeInput: true,
+    hoverOnly: true
 });
