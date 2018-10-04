42d63ae27ac5c7dd15ca7c9cea1c6035b7a7748f
caskd <caskd@gmx.de>
2018-10-04 15:42:22 +0200

    Better spacing and do not select text

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
 $(document).ready(function () {
-    $("head").append(
-        '<link rel="stylesheet" href="css/rules.css">' +
-        '<link rel="stylesheet" href="css/responsive.css">' +
-        '<link rel="stylesheet" href="css/animations.css">'
-    );
-    $('#abt-btn').click(function () {
-        $('#abt').toggleClass('hide');
-        $('.h-hide').toggleClass('h-on');
-        LoadOnce();
-    });
-    $('#close').click(function () {
-        $('#abt').addClass('hide');
-        $('.h-hide').removeClass('h-on');
-    });
-    $('.tab').click(function () {
-        var tab_id = $(this).attr('data-tab');
-        $('div.tabs a').removeClass('chosen');
-        $('.tab-content').addClass('hide');
-        $(this).addClass('chosen');
-        $('#' + tab_id).removeClass('hide');
-    });
+	$("head").append(
+		'<link rel="stylesheet" href="css/rules.css">' +
+		'<link rel="stylesheet" href="css/responsive.css">' +
+		'<link rel="stylesheet" href="css/animations.css">'
+	);
+	$('#abt-btn').click(function () {
+		$('#abt').toggleClass('hide');
+		$('.h-hide').toggleClass('h-on');
+		LoadOnce();
+	});
+	$('#close').click(function () {
+		$('#abt').addClass('hide');
+		$('.h-hide').removeClass('h-on');
+	});
+	$('.tab').click(function () {
+		var tab_id = $(this).attr('data-tab');
+		$('div.tabs a').removeClass('chosen');
+		$('.tab-content').addClass('hide');
+		$(this).addClass('chosen');
+		$('#' + tab_id).removeClass('hide');
+	});
 });
 
 function LoadOnce() {
-    $('#content').load('ajax/about.html');
-    $('.tab-content').addClass('hide');
-    LoadOnce = function () {};
+	$('#content').load('ajax/about.html');
+	$('.tab-content').addClass('hide');
+	LoadOnce = function () {};
 }
 
 var scene = document.getElementById('bg');
 var parallaxInstance = new Parallax(scene, {
-    relativeInput: true,
-    hoverOnly: true
+	relativeInput: true,
+	hoverOnly: true
 });
